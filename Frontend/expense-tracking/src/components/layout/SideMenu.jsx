b0ee7700deafa86a6f01eb99cfbce1c75ad22692
Eyobed <eyobedshawul@gmail.com>
2025-03-07 17:10:56 +0300

    sidemenu added

--- Frontend/expense-tracking/src/components/layout/SideMenu.jsx
+++ Frontend/expense-tracking/src/components/layout/SideMenu.jsx
@@ -1,11 +1,27 @@
-import React, { useContext } from 'react'
-import { SIDE_MENU_DATA } from '../../utils/data'
-import { userContext } from '../../context/userContext'
+import React, { useContext } from 'react';
+import { SIDE_MENU_DATA } from '../../utils/data';
+import { userContext } from '../../context/userContext';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SideMenu = () => {
     const { user, clearUser } = useContext(userContext);
 
     const navigate = useNavigate();
+
+    const handleClick = (path) => {
+        if(path === 'logout') {
+            handleLogout();
+            return;
+        }
+        navigate(path);
+    }
+
+    const handleLogout = () => {
+        clearUser();
+        localStorage.removeItem('token');
+        navigate('/login');
+    }
+
   return (
     <div>SideMenu</div>
   )
--- Frontend/expense-tracking/src/utils/data.js
+++ Frontend/expense-tracking/src/utils/data.js
@@ -1,5 +1,5 @@
 import {
-    LuLayOutDashboard,
+    LuLayoutDashboard,
     LuHandCoins,
     LuWalletMinimal,
     LuLogOut,
@@ -10,7 +10,7 @@ export const SIDE_MENU_DATA = [
     {
         id: 1,
         title: 'Dashboard',
-        icon: LuLayOutDashboard,
+        icon: LuLayoutDashboard,
         path: '/dashboard',
     },
     {
