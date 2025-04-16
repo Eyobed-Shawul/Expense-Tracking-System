72ab8894a5d9b80c4e3aa009e5e0822adab0b631
Eyobed <eyobedshawul@gmail.com>
2025-04-16 11:25:41 +0300

    change

--- Frontend/expense-tracking/src/pages/Dashboard/Home.jsx
+++ Frontend/expense-tracking/src/pages/Dashboard/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
   <DashboardLayout activeMenu="Dashboard">
     <div className="my-5 mx-auto">
-      Home new 
+     Home Page
     </div>
   </DashboardLayout>
 );
